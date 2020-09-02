d041aaf4839f28a56b66d743dcb21f6aa3e9aa6f
nasir sotudeh <nasir.sotudeh@gmail.com>
2020-09-02 21:32:33 +0430

    fix navbar

---
+++ blog/templatetags/base_tags.py
@@ -0,0 +1,15 @@
+from django import template
+from ..models import Category
+
+register = template.Library()
+
+@register.simple_tag
+def title(data="django"):
+     return data
+
+
+@register.inclusion_tag("blog/partials/navbar.html")
+def nav_category():
+     return {
+          "category": Category.objects.filter(status = True)
+     }
